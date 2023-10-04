2b473f02f69655cba6ca094a88499897912f0f33
melokb2023 <kyle.melo@lccdo.edu.ph>
2023-10-04 08:25:26 +0800

    main

---
+++ resources/views/components/authentication-card.blade.php
@@ -0,0 +1,9 @@
+<div class="bg-blue-100">
+    <div>
+        {{ $logo }}
+    </div>
+
+    <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-blue shadow-md overflow-hidden sm:rounded-lg">
+        {{ $slot }}
+    </div>
+</div>
